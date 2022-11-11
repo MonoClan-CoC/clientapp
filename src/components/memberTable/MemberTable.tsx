4325934b0ac7c6412dd6c5f8f5097af06cf5e5a6
Lucas <lopez.lucas@hotmail.com>
2022-11-11 21:33:35 +0100

    Realm provider and graphql link

--- src/components/memberTable/MemberTable.tsx
+++ src/components/memberTable/MemberTable.tsx
@@ -1,97 +1,132 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import './MemberTable.css';
-import {Repository} from "../../services/repo";
+import { Repository } from "../../services/repo";
+import { useQuery } from "@apollo/client";
+import {gql} from "graphql-tag";
+
+
+// @ts-ignore
+const CLAN_DETAIL = gql`
+    query {
+  clansDetail {
+    _id
+\t\tclanLevel
+\t\tclanPoints
+\t\tclanVersusPoints
+\t\tdescription
+\t\tisWarLogPublic
+\t\tmembers
+\t\tname
+\t\trequiredTownhallLevel
+\t\trequiredTrophies
+\t\trequiredVersusTrophies
+\t\ttag
+\t\ttype
+\t\twarFrequency
+\t\twarWinStreak
+\t\twarWins
+  }
+}
+`;
 
 function MemberTable() {
 
-    const [clanDetails, setClanDetail] = useState<any>();
-    const [selectedMember, setSelectedMember] = useState<any>();
-    const repo = new Repository();
+  const [clanDetails, setClanDetail] = useState<any>();
+  const [selectedMember, setSelectedMember] = useState<any>();
+  const repo = new Repository();
 
-    useEffect(() => {
-        repo.fetchData();
-    }, [])
+  const { loading, error, data } = useQuery(CLAN_DETAIL);
 
-    return (
-        <>
-            {clanDetails &&
-                <div className="memberContainer">
-                    <table className="tableContainer">
-                        <thead>
-                        <tr>
-                            <th>
-                                Rang
-                            </th>
-                            <th>
-                                Pseudo
-                            </th>
-                            <th>
-                                Niveau
-                            </th>
-                            <th>
-                                Role
-                            </th>
-                            <th>
-                                Ligue
-                            </th>
-                            <th>
-                                Trophées
-                            </th>
-                        </tr>
-                        </thead>
-                        <tbody>
-                        {clanDetails?.memberList?.map((member: any) =>
-                            <tr className="row" onClick={() => setSelectedMember(member)}>
-                                <td>
-                                    #{member.clanRank}
-                                </td>
-                                <td>
-                                    {member.name}
-                                </td>
-                                <td>
-                                    {member.expLevel}
-                                </td>
-                                <td className={member.role === 'leader' ? 'leaderCase' : member.role === 'coLeader' ? 'coLeaderCase' : ''}>
-                                    {member.role}
-                                </td>
-                                <td>
-                                    <img width={25} src={member.league?.iconUrls?.small}/>
-                                </td>
-                                <td>
-                                    {member.trophies}
-                                </td>
-                            </tr>
-                        )}
-                        </tbody>
-                    </table>
-                    {
-                        selectedMember &&
-                        <div className="memberDetails">
-                            <span className="memberTitle">{selectedMember.name}</span>
-                            <small>{selectedMember.tag}</small>
 
-                            <div className="memberSubtitle">
-                                <span>Troupes envoyées: {selectedMember.donations} Troupes reçues: {selectedMember.donationsReceived}</span>
-                            </div>
+  useEffect(() => {
+    repo.fetchData();
+  }, [])
 
-                        </div>
-                    }
+  if (data) {
+    return <pre>{ JSON.stringify(data,null,2) }</pre>
+  }
 
-                </div>
-            }
-            {
-                !clanDetails &&
-                <div className="lds-container">
-                    <div className="lds-ring">
-                        <div></div>
-                        <div></div>
-                        <div></div>
-                        <div></div>
-                    </div>
-                </div>
-            }
-        </>
-    );
+  return (
+    <>
+      { clanDetails &&
+      <div className="memberContainer">
+          <table className="tableContainer">
+              <thead>
+              <tr>
+                  <th>
+                      Rang
+                  </th>
+                  <th>
+                      Pseudo
+                  </th>
+                  <th>
+                      Niveau
+                  </th>
+                  <th>
+                      Role
+                  </th>
+                  <th>
+                      Ligue
+                  </th>
+                  <th>
+                      Trophées
+                  </th>
+              </tr>
+              </thead>
+              <tbody>
+              { clanDetails?.memberList?.map((member: any) =>
+                <tr className="row" onClick={ () => setSelectedMember(member) }>
+                  <td>
+                    #{ member.clanRank }
+                  </td>
+                  <td>
+                    { member.name }
+                  </td>
+                  <td>
+                    { member.expLevel }
+                  </td>
+                  <td
+                    className={ member.role === 'leader' ? 'leaderCase' : member.role === 'coLeader' ? 'coLeaderCase' : '' }>
+                    { member.role }
+                  </td>
+                  <td>
+                    <img width={ 25 } src={ member.league?.iconUrls?.small }/>
+                  </td>
+                  <td>
+                    { member.trophies }
+                  </td>
+                </tr>
+              ) }
+              </tbody>
+          </table>
+        {
+          selectedMember &&
+          <div className="memberDetails">
+              <span className="memberTitle">{ selectedMember.name }</span>
+              <small>{ selectedMember.tag }</small>
+
+              <div className="memberSubtitle">
+                  <span>Troupes envoyées: { selectedMember.donations } Troupes reçues: { selectedMember.donationsReceived }</span>
+              </div>
+
+          </div>
+        }
+
+      </div>
+      }
+      {
+        !clanDetails &&
+        <div className="lds-container">
+            <div className="lds-ring">
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+            </div>
+        </div>
+      }
+    </>
+  );
 }
 
 export default MemberTable;
--- src/services/repo.ts
+++ src/services/repo.ts
@@ -1,34 +1,35 @@
-import axios from 'axios'
-
 export class Repository {
-    private data: string = JSON.stringify({
-        "collection": "ClansDetails",
-        "database": "MonoClan",
-        "dataSource": "Cluster0"
-    });
+  private data: string = JSON.stringify({
+    "collection": "ClansDetails",
+    "database": "MonoClan",
+    "dataSource": "Cluster0"
+  });
 
-    private config: any = {
-        method: 'post',
-        url: 'https://data.mongodb-api.com/app/data-lfpip/endpoint/data/v1/action/findOne',
-        headers: {
-            'Content-Type': 'application/json',
-            'Access-Control-Request-Headers': '*',
-            'api-key': 'iEcKHLItZw5i2q81DzQzKrNFxO3Lv5dcZJHvUh2E5Ykv9yIrfbM8bqdHbqjKf8d7',
-        },
-        data: this.data
-    };
+  private config: any = {
+    method: 'post',
+    url: 'https://realm.mongodb.com/api/client/v2.0/app/data-lfpip/graphql',
+    headers: {
+      'Content-Type': 'application/json',
+      'Access-Control-Request-Headers': '*',
+      'api-key': 'iEcKHLItZw5i2q81DzQzKrNFxO3Lv5dcZJHvUh2E5Ykv9yIrfbM8bqdHbqjKf8d7',
+      'Access-Control-Allow-Origin': 'realm.mongodb.com'
+    },
+    data: this.data
+  };
 
-    public constructor() {
-    }
+  public constructor() {
+  }
 
-    public fetchData(): void {
-        axios(this.config)
-            .then(function (response) {
-                console.log(JSON.stringify(response.data));
-            })
-            .catch(function (error) {
-                console.log(error);
-            });
-    }
+  public fetchData(): void {
+    /*
+            axios(this.config)
+                .then(function (response) {
+                    console.log(JSON.stringify(response.data));
+                })
+                .catch(function (error) {
+                    console.log(error);
+                });
+    */
+  }
 
 }
