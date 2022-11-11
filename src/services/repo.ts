fc5f91356da98398e6d91e258b5af2b75d79acd7
Alex <47835444+buenoC6@users.noreply.github.com>
2022-11-11 20:58:31 +0100

    change request

--- src/services/repo.ts
+++ src/services/repo.ts
@@ -2,11 +2,44 @@ import axios from 'axios'
 
 export class Repository {
 
-    private data: string = JSON.stringify({
-        "collection": "ClansDetails",
-        "database": "MonoClan",
-        "dataSource": "Cluster0"
-    });
+    // private data: string = JSON.stringify({
+    //     "collection": "ClansDetails",
+    //     "database": "MonoClan",
+    //     "dataSource": "Cluster0"
+    // });
+
+    private data: string = "query {\n" +
+        "  clansDetail {\n" +
+        "    _id\n" +
+        "\t\tclanLevel\n" +
+        "\t\tclanPoints\n" +
+        "\t\tclanVersusPoints\n" +
+        "\t\tdescription\n" +
+        "\t\tisWarLogPublic\n" +
+        "\t\tmembers\n" +
+        "    memberList {\n" +
+        "      clanRank\n" +
+        "      donations\n" +
+        "      donationsReceived\n" +
+        "      expLevel\n" +
+        "      name\n" +
+        "      previousClanRank\n" +
+        "      role\n" +
+        "      tag\n" +
+        "      trophies\n" +
+        "      versusTrophies\n" +
+        "    }\n" +
+        "\t\tname\n" +
+        "\t\trequiredTownhallLevel\n" +
+        "\t\trequiredTrophies\n" +
+        "\t\trequiredVersusTrophies\n" +
+        "\t\ttag\n" +
+        "\t\ttype\n" +
+        "\t\twarFrequency\n" +
+        "\t\twarWinStreak\n" +
+        "\t\twarWins\n" +
+        "  }\n" +
+        "}";
 
     private config: any = {
         method: 'post',
@@ -14,8 +47,8 @@ export class Repository {
         headers: {
             'Content-Type': 'application/json',
             'Access-Control-Request-Headers': '*',
-            //"Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c",
-            'apikey': 'kwUbfBPnawSfNzgzWOrZwM5g4IyAGQmH2nTYmYUuDn8CjYtgbWiTkB9hEnsnEWUl',
+            "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c",
+            //'apikey': 'kwUbfBPnawSfNzgzWOrZwM5g4IyAGQmH2nTYmYUuDn8CjYtgbWiTkB9hEnsnEWUl',
             'Access-Control-Allow-Origin':'*'
         },
         data: this.data
